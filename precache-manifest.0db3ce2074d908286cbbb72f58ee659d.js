3eed8aef6079b3dd6b6484d14b34c02bbc6175a4
mehedi23 <satusatusatu0.com>
2020-09-01 12:56:30 +0600

    Updates

---
+++ precache-manifest.0db3ce2074d908286cbbb72f58ee659d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6167dd8258e5c85bd9067f513b7dba7c",
+    "url": "/twitter-clone.git/index.html"
+  },
+  {
+    "revision": "20395981e0cc2d70bd92",
+    "url": "/twitter-clone.git/static/css/main.584c8bf2.chunk.css"
+  },
+  {
+    "revision": "4fb17f15019c7b0dd565",
+    "url": "/twitter-clone.git/static/js/2.ef4e6285.chunk.js"
+  },
+  {
+    "revision": "698c7d1ab7fc994ffbdcf6ff99e91ff0",
+    "url": "/twitter-clone.git/static/js/2.ef4e6285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20395981e0cc2d70bd92",
+    "url": "/twitter-clone.git/static/js/main.f9ffebf1.chunk.js"
+  },
+  {
+    "revision": "c7d82d947df87683f9fb",
+    "url": "/twitter-clone.git/static/js/runtime-main.d028d553.js"
+  }
+]);
